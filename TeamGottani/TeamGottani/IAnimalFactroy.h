c26244d3d67509f72b1938b80a0e0532e0de179f
KoiiroHikanyan <hikanyan0131@gmail.com>
2024-10-12 12:31:46 +0900

    Update : インターフェース実装

--- TeamGottani/TeamGottani/IAnimalFactroy.h
+++ TeamGottani/TeamGottani/IAnimalFactroy.h
@@ -1,7 +1,8 @@
 ﻿#pragma once
 
-class IAnimalFactroy
-{
+// IAnimalFactoryインターフェース
+class IAnimalFactory {
 public:
-	
+	virtual ~IAnimalFactory() = default;
+	virtual std::unique_ptr<Animal> createAnimal() const = 0;
 };
