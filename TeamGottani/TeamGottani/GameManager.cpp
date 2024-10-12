5ee2f03c3ba1da0214037eebc6de93e8e547e24f
KoiiroHikanyan <hikanyan0131@gmail.com>
2024-10-12 16:15:19 +0900

    Update : GameManagerにPlayerをインスタンス化

--- TeamGottani/TeamGottani/GameManager.cpp
+++ TeamGottani/TeamGottani/GameManager.cpp
@@ -1,2 +1,34 @@
 ﻿#include "stdafx.h"
 #include "GameManager.h"
+// GameManagerのコンストラクタ
+GameManager::GameManager()
+	: _factory(nullptr), _player(nullptr) // 初期化リストでメンバ変数を初期化
+{
+}
+
+// GameManagerのデストラクタ
+GameManager::~GameManager()
+{
+	// 必要なクリーンアップ処理があればここに書く
+}
+
+void GameManager::Awake()
+{
+	// 初期化処理
+}
+
+void GameManager::Start()
+{
+	// ゲーム開始処理
+	_player = std::make_unique<Player>();
+}
+
+void GameManager::Update()
+{
+	// 毎フレームの更新処理
+}
+
+void GameManager::Draw()
+{
+	// 描画処理
+}
