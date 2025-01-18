c3cbc1a415e4fc5dbacfcb4eaeb58849e82a4d8e
イワダレリュウゴ <106290660+IwadareRyu@users.noreply.github.com>
2025-01-18 11:55:30 +0900

    [Add]今までのプログラム追加

---
+++ TeamGottani/TeamGottani/Iwadare/Button.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+class Button
+{
+public:
+	Button(Rect buttonObj,Color color,String str);
+	~Button() {};
+	bool ButtonClicked();
+	void Draw(Font buttonFont) const;
+	Rect m_buttonObj;
+	Color m_buttonNormalColor;
+	Color m_buttonOverColor;
+	Color m_buttonClickColor;
+	String buttonStr;
+};
+
